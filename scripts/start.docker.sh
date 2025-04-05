a1ec227a9fdc53578a403b18aaa036a713f6792d
Wachirawat Chaihanij <wachirawat.chaihanij@dga.or.th>
2025-04-05 11:41:41 +0700

    fixed code

--- scripts/start.docker.sh
+++ scripts/start.docker.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-set -e
-PORT=3000 \
-MONGODB_URL="mongodb+srv://savanfinfin-dev:OE4tAeHrzxCWgJzz@savan-fin-fin-developme.vsl50.mongodb.net/savan-finfin-development" \
-PUBLIC_KEY=$(cat resources/keys/public.key) \
-PRIVATE_KEY=$(cat resources/keys/private.key) \
-AWS_ACCESS_KEY=AKIA56SZP5WB7BALDV5G \
-AWS_SECRET_ACCESS_KEY=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 \
-AWS_REGION=ap-southeast-1 \
-AWS_BUCKET_NAME=savan-finfin-develop \
-npm run start:dev
-
 docker run -it --rm -p 3000:3000 \
  -e PORT=3000 \
  -e MONGODB_URL="mongodb+srv://savanfinfin-dev:OE4tAeHrzxCWgJzz@savan-fin-fin-developme.vsl50.mongodb.net/savan-finfin-development" \
