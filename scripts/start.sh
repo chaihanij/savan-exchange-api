8346b0e1ee822925a57e56d7999dd162bded3d93
Wachirawat Chaihanij <wachirawat.chaihanij@dga.or.th>
2025-04-12 19:12:59 +0700

    add railway cd

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,8 @@ PORT=3000 \
 MONGODB_URL="mongodb+srv://admin:eYIcepkCm9w5bpSf@savan-ex.zcug2wh.mongodb.net/savan-ex" \
 PUBLIC_KEY=$(cat resources/keys/public.key) \
 PRIVATE_KEY=$(cat resources/keys/private.key) \
-AWS_ACCESS_KEY=AKIA56SZP5WB7BALDV5G \
-AWS_SECRET_ACCESS_KEY=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 \
+AWS_ACCESS_KEY=AKIAYT3NOVDEHGYE6AP3 \
+AWS_SECRET_ACCESS_KEY=W2NWOqWFiSPdoqED2SM4yTuYLqslz \
 AWS_REGION=ap-southeast-1 \
-AWS_BUCKET_NAME=savan-exchange-develop \
+AWS_BUCKET_NAME=savan-ex-develop \
 npm run start:dev
