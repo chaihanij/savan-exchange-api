585243350f91eb8dab37a028bfcc2d10ebc43599
Wachirawat Chaihanij <wachirawat.chaihanij@dga.or.th>
2025-04-19 19:33:11 +0700

    update code

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-ECS_TASK_DEFINITION_PATH=ecs-task-definition.json
-
-if [ ! -f "$ECS_TASK_DEFINITION_PATH" ]; then
-  echo "ECS task definition file not found: $ECS_TASK_DEFINITION_PATH"
-  exit 1
-fi
